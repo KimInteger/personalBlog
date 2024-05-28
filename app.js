e39e2551919732f2c5fb0a76e4dd4b98d99c46ca
KimInteger <kjs20151240@gmail.com>
2024-05-28 09:23:56 +0900

    today함수 수정. 한자리수일경우 앞에 0을 붙이기 위해서 백틱으로 감싸고 00을 추가함

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ function today(){
   const date = new Date;
   const year = date.getFullYear();
   const month = `00${date.getMonth()+1}`.slice(-2);
-  const day = date.getDate();
+  const day = `00${date.getDate()}`.slice(-2);
   const hour = `00${date.getHours()}`.slice(-2);
-  const minute = date.getMinutes();
-  const sec = date.getSeconds();
+  const minute = `00${date.getMinutes()}`.slice(-2);
+  const sec = `00${date.getSeconds()}`.slice(-2);
   today = `${year}-${month}-${day}-${hour}-${minute}-${sec}`;
   return today;
 }
