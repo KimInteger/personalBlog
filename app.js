1f481c3894022e1e577d0473cbaa9af9deadf72e
KimInteger <kjs20151240@gmail.com>
2024-05-28 14:33:52 +0900

    readdir로 읽어온 데이터가 객체였음을 확인.

--- app.js
+++ app.js
@@ -132,12 +132,12 @@ const mainTemp = function makeMain(content){
 };
 
 // ! 폴더를 읽어내서 변수에 할당하기.
-let folderData;
-fs.readdir(path.join(__dirname,'public','writeFile'),(err,data)=>{
+let folderData = [];
+fs.readdir(path.join(__dirname,'public','writeFile'),'utf8',(err,data)=>{
   if(err){
     console.error("에러가 발생했습니다!" ,err);
   }
-  folderData = data;
+  console.log(typeof(data));
 });
 
 console.log(folderData);
