91fd9885c70cd4843512c0e6ac0c372e9649982c
KimInteger <kjs20151240@gmail.com>
2024-05-30 16:46:47 +0900

    js할당 끝 확인까지 끝마침

--- public/createForm.js
+++ public/createForm.js
@@ -10,24 +10,32 @@ function changePage(){
 
   let labelTitle = document.createElement('label');
   labelTitle.setAttribute('for','title');
+  labelTitle.textContent = '제목'
+  labelTitle.style.display = 'block'
 
   let labelContent = document.createElement('label');
   labelContent.setAttribute('for','content');
+  labelContent.textContent = '내용'
+  labelContent.style.display = 'block'
 
   let inputTitle = document.createElement('input');
   inputTitle.id = 'title';
   inputTitle.name = 'title';
   inputTitle.type = 'text';
+  inputTitle.style.display = 'block'
 
   let contentArea = document.createElement('textarea');
   contentArea.id = 'content';
   contentArea.name = 'content';
   contentArea.setAttribute('rows',15);
   contentArea.setAttribute('cols',25);
+  contentArea.style.display = 'block'
 
   let inputSubmit = document.createElement('input');
   inputSubmit.setAttribute('type','submit');
   inputSubmit.setAttribute('value','글쓰기');
+  inputSubmit.style.display = 'block'
+  inputSubmit.style.marginTop = '10px'
 
   form.appendChild(labelTitle)
   form.appendChild(inputTitle)
@@ -35,10 +43,11 @@ function changePage(){
   form.appendChild(contentArea)
   form.appendChild(inputSubmit)
 
-  
+
 
   root.textContent = '';
+  root.appendChild(form);
 };
 
 
-modify.addEventListener('click',()=>{});
+modify.addEventListener('click',changePage);
