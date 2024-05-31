fdb757fa498662586e38a91ac28352591d444055
KimInteger <kjs20151240@gmail.com>
2024-05-31 17:21:38 +0900

    temp.js에 writeField를 추가

--- memoModule/temp.js
+++ memoModule/temp.js
@@ -39,15 +39,8 @@ const temp = {
         <title>이것은블로그입니다.</title>
       </head>
       <body>
-        <form action="/create" method="POST" id="form">
-          <label for="title">제목</label><br>
-          <input type="text" id="title" name="title"><br><br>
-          <label for="content">내용</label><br>
-          <textarea name="content" id="content" cols="25" rows="15"></textarea><br><br>
-          <button id="send" type="submit">글쓰기</button>
-        </form>
         <div>
-          <h2>패치노트</h2>
+          <h1>패치노트</h1>
           <ul id="board">
           ${content}
           </ul>
@@ -75,7 +68,27 @@ const temp = {
         <button id="send" type="submit">글쓰기</button>
       </form>
     </body>
-  </html>`
+  </html>`,
+
+  writeField : `
+  <!DOCTYPE html>
+  <html lang="ko">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>글쓰기</title>
+  </head>
+  <body>
+    <form action="/create" method="POST" id="form">
+      <label for="title">제목</label><br>
+      <input type="text" id="title" name="title"><br><br>
+      <label for="content">내용</label><br>
+      <textarea name="content" id="content" cols="25" rows="15"></textarea><br><br>
+      <button id="send" type="submit">글쓰기</button>
+    </form>
+  </body>
+  </html>
+  `
 }
 
 module.exports = temp;
