d101a7c428caffd267af1c7b62ef4bc4c40e34a3
KimInteger <kjs20151240@gmail.com>
2024-05-31 12:11:23 +0900

    날짜함수를 호출하여 사용에 문제없음을 확인

--- app.js
+++ app.js
@@ -8,20 +8,8 @@ const qs = require('node:querystring');
 
 const path = require('node:path');
 
-// ! 기점을 잡기위한 날짜 new Date매서드
+const today = require('./memoModule/nowDate');
 
-function today(){
-  let today = '';
-  const date = new Date;
-  const year = date.getFullYear();
-  const month = `00${date.getMonth()+1}`.slice(-2);
-  const day = `00${date.getDate()}`.slice(-2);
-  const hour = `00${date.getHours()}`.slice(-2);
-  const minute = `00${date.getMinutes()}`.slice(-2);
-  const sec = `00${date.getSeconds()}`.slice(-2);
-  today = `${year}-${month}-${day}-${hour}-${minute}-${sec}`;
-  return today;
-}
 
 // ! server연결을 원활하게 하기 위한 기명함수 제작.
 
